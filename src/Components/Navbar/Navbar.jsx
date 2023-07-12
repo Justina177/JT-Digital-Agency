3cc5ceced44afccba4476b903e0c9616d2d4adbb
justina177 <justinaenenche@yahoo.com>
2023-07-12 19:49:41 +0100

    initial commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 50px;
+`
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-evenly;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <span>Logo</span>
+            <span>button</span>
+        </Wrapper>
+
+        n
+    </Container>
+  )
+}
+
+export default Navbar
