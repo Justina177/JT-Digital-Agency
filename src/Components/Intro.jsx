2e3ee26cc99418decbaf8dba18a30f0ecaa60993
justina177 <justinaenenche@yahoo.com>
2023-07-13 02:02:19 +0100

    Added the Leftside Component in Intro component

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-    height: calc(100vh - 50px);
-    padding: 20px;
+  height: calc(100vh - 50px);
+  padding: 20px;
 `;
 
-const Left =styled.div`
-    width: 60%;
+const Left = styled.div`
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Title = styled.h1`
-width: 60%;
+  width: 60%;
   font-size: 60px;
 `;
 
@@ -20,8 +24,43 @@ const Desc = styled.p`
   font-size: 20px;
   margin-top: 20px;
 `
+
+const Info = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const Button = styled.button`
+padding: 15px;
+background-color: darkblue;
+color: white;
+border-radius: 10px;
+font-weight: bold;
+border: none;
+letter-spacing: 2px;
+cursor: pointer;
+`;
+
+const Contact = styled.div`
+display: flex;
+flex-direction: column
+`;
+
+const Phone = styled.span`
+color: #f0667d;
+font-weight: bold;
+`;
+
+const ContactText = styled.span`
+color: gray;
+margin-top: 5px;
+`;
+
+
 const Right = styled.div`
-    width: 40%;
+  width: 40%;
 `;
 
 
@@ -37,6 +76,14 @@ const Intro = () => {
           our clients is the only way to have a real 
           impact on their business
         </Desc>
+        <Info>
+        <Button>START A PROJECT</Button>
+        <Contact>
+          <Phone>Call Us (234) 132-2206</Phone>
+          <ContactText>For any question or concern</ContactText>
+        </Contact>
+           
+        </Info>
       </Left>
       <Right></Right>
     </Container>
