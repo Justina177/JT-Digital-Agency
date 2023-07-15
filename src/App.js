b54a579977bfb6266d3710a73f8ee6d02c4414dc
justina177 <justinaenenche@yahoo.com>
2023-07-15 07:38:58 +0100

    Added Featured Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import './App.css';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import Featured from './Components/Featured';
 import Intro from './Components/Intro';
 import Navbar from './Components/Navbar/Navbar';
 
@@ -8,19 +9,25 @@ height: 100vh;
 overflow: hidden;
 position: relative;
 `;
-
-const IntoShape = styled.div`
+const Shape = css`
 width: 100%;
 height: 100%;
 position: absolute;
 top: 0;
 left: 0;
 z-index: -1;
-clip-path: polygon(67%, 0%, 100% 0%, 100% 100%, 55% 100%);
-background-color: crimson;
+`
 
-clip-path: polygon(67% 0, 100% 0%, 100% 100%, 55% 100%);
+const IntoShape = styled.div`
+${Shape};
+clip-path: polygon(64% 0, 100% 0%, 100% 100%, 55% 100%);
   background-color: crimson;
+`
+
+const FeaturedShape = styled.div`
+${Shape};
+clip-path: polygon(0 0, 55% 0%, 33% 100%, 0% 100%);
+  background-color: pink;
 `
 
 function App() {
@@ -31,6 +38,10 @@ function App() {
       <Intro />
       <IntoShape />
       </Container> 
+      <Container>
+      <Featured />
+      <FeaturedShape />
+      </Container>
       
     
       
--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -97,26 +97,6 @@ const Intro = () => {
       </Right>
       <AnimatedShapes />
     </Container>
-
-    /* <Container>
-      <Left>
-        <Title>Adventures in creative age</Title>
-        <Desc>
-          We believe that designing products and services in close partnership
-          with our clients is the only way to have a real impact on their
-          business.
-        </Desc>
-        <Info>
-          <Button>START A PROJECT</Button>
-          <Contact>
-            <Phone>Call Us (012) 345 - 6789</Phone>
-            <ContactText>For any question or concern</ContactText>
-          </Contact>
-        </Info>
-      </Left>
-      <Right><Image src={img1}/></Right>
-      
-    </Container> */
   )
 }
 
