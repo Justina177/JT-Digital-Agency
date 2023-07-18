244eff7829bcf36838dfa09214679de3efbc1981
justina177 <justinaenenche@yahoo.com>
2023-07-18 03:50:38 +0100

    Added price and PriceCard component

--- src/App.js
+++ src/App.js
@@ -3,61 +3,72 @@ import styled, { css } from 'styled-components';
 import Featured from './Components/Featured';
 import Intro from './Components/Intro';
 import Navbar from './Components/Navbar/Navbar';
+import Price from './Components/Price';
 import Service from './Components/Service';
 
 const Container = styled.div`
-height: 100vh;
-overflow: hidden;
-position: relative;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
 `;
 const Shape = css`
-width: 100%;
-height: 100%;
-position: absolute;
-top: 0;
-left: 0;
-z-index: -1;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
 `
 
 const IntoShape = styled.div`
-${Shape};
-clip-path: polygon(64% 0, 100% 0%, 100% 100%, 55% 100%);
+  ${Shape};
+  clip-path: polygon(64% 0, 100% 0%, 100% 100%, 55% 100%);
   background-color: crimson;
 `
 
 const FeaturedShape = styled.div`
-${Shape};
-clip-path: polygon(0 0, 55% 0%, 33% 100%, 0% 100%);
-background-color :pink ;
+  ${Shape};
+  clip-path: polygon(0 0, 55% 0%, 33% 100%, 0% 100%);
+  background-color :pink ;
 `;
 
 const ServiceShape = styled.div`
-${Shape}
-clip-path: polygon(0 0, 33% 0, 33% 100%, 0 100%);
-background-color: #f88497;
-`
+  ${Shape}
+  clip-path: polygon(0 0, 33% 0, 33% 100%, 0 100%);
+  background-color: #f88497;
+`;
+
+const PriceShape = styled.div`
+  ${Shape}
+  clip-path: polygon(33% 0, 100% 0%, 100% 100%, 67% 100%);
+  background-color: crimson;
+`;
+
 
 function App() {
   return (
     <div className="container">
       <Container>
-      <Navbar />
-      <Intro />
-      <IntoShape />
+        <Navbar />
+        <Intro />
+        <IntoShape />
       </Container> 
+
       <Container>
-      <Featured />
-      <FeaturedShape />
+        <Featured />
+        <FeaturedShape />
       </Container>
+
       <Container>
-      <Service />
-      <ServiceShape />
+        <Service />
+        <ServiceShape />
       </Container>
-      
-    
-      
+
+      <Container>
+        <Price/>
+        <PriceShape/>
+      </Container>
     </div>
-    // 54:08
   );
 }
 
